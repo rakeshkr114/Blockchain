399782b33799478c6fa0e8be8e359a1798cb65a4
Rakesh kumar Yadav <rakeshkr114@gmail.com>
2019-04-04 10:01:00 +0000

    Added code for wallets

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const DIFFICULTY = 3;   //Default difficulty level
 const MINE_RATE = 3000; //in milliseconds (3 sec)
+const INITIAL_BALANCE = 500;
 
-module.exports = {DIFFICULTY, MINE_RATE};
+module.exports = {DIFFICULTY, MINE_RATE, INITIAL_BALANCE};
--- dev-test.js
+++ dev-test.js
@@ -11,10 +11,16 @@ console.log(fooBlock.toString());
 
 */
 
-
+/*
 const Blockchain = require('./blockchain');
 const bc = new Blockchain();
 
 for(let i=0; i<10; i++){
     console.log(bc.addBlock(`duh ${i}`).toString());
 }
+*/
+
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+
+console.log(wallet.toString());
