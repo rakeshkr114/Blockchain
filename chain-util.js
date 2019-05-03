adb88918f4a5b582c11804577dff1fec98fae369
Rakesh kumar Yadav <rakeshkr114@gmail.com>
2019-05-03 05:08:35 +0000

    added code to generate unique id

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,14 @@
+const uuidV1 = require('uuid/v1'); //Now uuidV1 is a function which generates an unique ID based on current timestamp
 const EC = require('elliptic').ec; //class takes one string argument :  what curve based cryptography implementation to use
 const ec = new EC("secp256k1");   //Standard of Efficient Cryptography Prime 256-bits K-kobletts(Mathametician) 1-Fisrt implementation of curve algo in this standard
     
 class ChainUtil {
     static genKeyPair(){
         return ec.genKeyPair();
+    }
+
+    static id(){
+        return uuidV1();    // call the fun. and return the ID
     }
 }
 
